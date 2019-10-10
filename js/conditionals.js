3bcc5f37705c5c1207f26c8dcd98d663396f82df
Roger Mayer <rmayer1984@gmail.com>
2019-10-10 10:26:28 -0500

    finished conditionals lecture

--- js/conditionals.js
+++ js/conditionals.js
@@ -39,6 +39,36 @@
 // }
 // toDo1(play);
 
+// instructer solved
+
+// var userConfrim = confirm("would you like to enter a number?");
+//
+// if(userConfrim){
+//     //true path
+//     var userInput = prompt("please enter a number");
+//     console.log("the users number: " + userInput);
+//
+//     //checking to see if usernumber is a number
+//     var isNumber = !isNaN(userInput);
+//     console.log("the user entered a number: " + isNumber);
+//
+//     if(isNumber){
+//         var userNumber = parseFloat(userInput);
+//         var isEven = userNumber % 2 ===0;
+//         console.log("the user number is even: "+ isEven);
+//
+//         var evenMessage = (isEven) ? "your number is even" : "your number is odd";
+//         alert(evenMessage);
+//
+//         alert(userNumber + 100);
+//
+//         var posNegativeMessage = (userNumber >= 0) ? "your number is positive" : "your number is negative";
+//         alert(posNegativeMessage);
+//
+//     } else {
+//         alert("not a number");
+//     }
+// }
 
 
 /* ########################################################################## */
@@ -77,6 +107,9 @@
     // console.log(analyzeColor(color));
     // alert(analyzeColor(color));
 
+
+
+
 // Don't change the next two lines!
 // These lines create two variables for you:
 // - `colors`: a list of the colors of the rainbow
@@ -84,12 +117,15 @@
 //                  will contain a different color everytime the page loads)
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+
 /**
  * TODO:
  * Pass the `randomColor` variable to your function and console.log the results.
  * You should see a different message everytime you refresh the page
  */
 // console.log(analyzeColor(randomColor));
+
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
@@ -144,22 +180,52 @@ alert(analyzeColor(userColor));
  * Test your function by passing it various values and checking for the expected
  * return value.
  */
-var cart = parseInt(prompt("Total cost of cart"));
-function calculateTotal(lucky, cart) {
-    if (lucky == 0){
-        return ("No discount- $" + cart);
-    } else if (lucky == 1) {
-        return ("10% discount- $" + (cart * .90));
-    } else if (lucky == 2) {
-        return ("25% discount- $" + (cart * .75));
-    } else if (lucky == 3) {
-    return ("35% discount- $" + (cart * .65));
-    } else if (lucky == 4) {
-        return ("50% discount- $" + (cart * .50));
-    } else if (lucky == 5) {
-    return ("It's free! you owe $" + (cart - cart));
+// var cart = parseInt(prompt("Total cost of cart"));
+// function calculateTotal(lucky, cart) {
+//     if (lucky == 0){
+//         return ("No discount- $" + cart);
+//     } else if (lucky == 1) {
+//         return ("10% discount- $" + (cart * .90));
+//     } else if (lucky == 2) {
+//         return ("25% discount- $" + (cart * .75));
+//     } else if (lucky == 3) {
+//     return ("35% discount- $" + (cart * .65));
+//     } else if (lucky == 4) {
+//         return ("50% discount- $" + (cart * .50));
+//     } else if (lucky == 5) {
+//     return ("It's free! you owe $" + (cart - cart));
+//     }
+// }
+//
+// instructor switch solution
+
+function calculateTotal(luckyNumber, totalPrice){
+    var discountRate = 0;
+
+        switch (luckyNumber) {
+            case 0:
+                discountRate = 0;
+                break;
+            case 2:
+                discountRate = .1;
+                break;
+            case 3:
+                discountRate = .25;
+                break;
+            case 4:
+                discountRate = .35;
+                break;
+            case 4:
+                discountRate = 0.5;
+                break;
+            case 5:
+                discountRate = 1;
+                break;
+        }
+        return totalPrice = (totalPrice + discountRate);
     }
-}
+
+
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 6.
@@ -169,4 +235,9 @@ function calculateTotal(lucky, cart) {
  */
 // Generate a random number between 0 and 6
 var luckyNumber = Math.floor(Math.random() * 6);
-alert(calculateTotal(luckyNumber, cart));
+var totalBillPrompt = parseFloat(prompt("what was total bill?"));
+console.log(luckyNumber);
+var finalAmount = calculateTotal((luckyNumber, totalBillPrompt));
+alert("your lucky number is: " + luckyNumber + "your total bill was: " + totalBillPrompt + "and your total discount is; " + discountRate);
+
+
