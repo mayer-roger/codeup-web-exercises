2c95017ec374e1222cc1c805ebc6ec46898cd571
Roger Mayer <rmayer1984@gmail.com>
2019-10-13 17:44:57 -0500

    modified loops/while

--- js/while.js
+++ js/while.js
@@ -6,12 +6,12 @@ while (i < 65537) {
     i *= 2;
 }
 
-// This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-var conesSold = Math.floor(Math.random() * 5) + 1;
-
+// // This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// // This expression will generate a random number between 1 and 5
+// var conesSold = Math.floor(Math.random() * 5) + 1;
+//
 // do {
 //     console.log (conesSold + ' cones sold...');
-//
+//     conesSold++;
 // } while (allCones > conesSold);
