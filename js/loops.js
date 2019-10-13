d0e02925275f6af29c64b5156e16fc78784b2eb3
Roger Mayer <rmayer1984@gmail.com>
2019-10-13 18:09:50 -0500

    stopped at random loop

--- js/loops.js
+++ js/loops.js
@@ -1,17 +1,23 @@
 "use strict";
 
 function  showMultiplicationTable(x) {
-    for (var i = 1; i <= 10; ++i) {
-        console.log(x + " * " + i + " = "+  (x * i));
+    for (var i = 1; i <= 10; i++) {
+        console.log(x + " * " + i + " = " + (x * i));
     }
 }
 showMultiplicationTable(7);
+
 
 // This is how you get a random number between 50 and 100
 var randomTwo = Math.floor(Math.random() * 180) + 20;
 // This expression will generate a random number between 1 and 5
 var tenRandom = Math.floor(Math.random() * 10) + 1;
 
-function evenOdd() {
 
+function evenOdd(randomTwo) {
+    if (randomTwo % 2 === 0){
+        return (randomTwo + " Is even");
+    } else {
+        return (randomTwo + " Is odd");
+    }
 }
