d95f6328c1ff1628eabf37ed7b7740cdd512ad93
Roger Mayer <rmayer1984@gmail.com>
2019-10-23 13:34:46 -0500

    mapbox API work

--- js/mapbox-api-lec.js
+++ js/mapbox-api-lec.js
@@ -1,8 +1,9 @@
 /**********************************************
  * 			SETTING UP KEYS.JS
  *********************************************/
-// TODO TOGETHER: Open .gitignore and add keys.js. Add keys.js file and import to mapbox html file. Your api keys are stored in keys.js and are added to the .gitignore so they are protected
+// TODO TOGETHER: Open .gitignore and add mapbox-api-lec.js. Add mapbox-api-lec.js file and import to mapbox html file. Your api keys are stored in mapbox-api-lec.js and are added to the .gitignore so they are protected
 
+console.log(mapboxToken);
 /**********************************************
  * 			CUSTOMIZING THE MAP
  *********************************************/
@@ -11,7 +12,14 @@
 // Zoom levels range from 0 up to 24, with 0 being a global view and 24 being the most detailed at street level (the max zoom level depends on the location).
 
 //TODO TOGETHER: Set map to san antonio area using the coordinates [-98.4916, 29.4252]
+mapboxgl.accessToken = mapboxToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    zoom: 14,
 
+    center: [-98.4916, 29.4252]
+});
 //TODO: Experiment with different map styles, zoom levels, and centers. You will need to reference the mapbox docs. (~15 minutes)
 
 
@@ -25,6 +33,19 @@
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-98.4916, 29.4260]. This marker will mark the Alamo on our map.
 // TODO TOGETHER: Change the color of the marker
+
+var markerOptions = {
+    color: "teal",
+    draggable: true
+
+}
+
+
+
+var marker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-98.4861, 29.4260])
+    .addTo(map);
+
 
 
 // TODO: Experiment with the color, and setting the LngLat
@@ -40,8 +61,22 @@
 
 // TODO TOGETHER: Add a popup to the map over codeup. Set the html as a paragraph that says "Codeup Rocks!"
 // TODO TOGETHER: Comment out the popup we just added. Add a popup to the alamo marker.
+var popupOptions = {
+    className: "popUpBox"
+}
+
+var popup = new mapboxgl.Popup(popupOptions)
+    .setLngLat([-98.489615, 29.426827])
+    .setHTML("<p>Codeup Rocks!</p>")
+    .addTo(map)
 
 
+
+var codeupPopup = new mapboxgl.Popup()
+    .setHTML("<p>Remember CodeUp!</p>")
+    .addTo(map)
+
+marker.setPopup(codeupPopup)
 // TODO: Review the popup docs. What are some additional options we can pass to the popup?
 // TODO: Try setting the text by using ".setText()" instead of ".setHTML()"
 
@@ -50,11 +85,27 @@
 /**********************************************
  * 					Geocoder
  *********************************************/
-// Geocoding Docs --> https://docs.mapbox.com/api/search/#geocoding
+//Geocoding Docs --> https://docs.mapbox.com/api/search/#geocoding
 
 
 // TODO TOGETHER: Using the Geocoder helper function, log the coordinates of Codeup and recenter the map to focus on Codeup. Comment out previous map code.
+// geocode("600 Navarro st #350, San Antonio, Tx 78205", mapboxToken).then(function(result){
+//     console.log(result);
+//     map.setCenter(result);
+//
+// })
+function geo(){
+    geocode("930 Everest, San Antonio, Tx 78209", mapboxToken).then(function(result) {
+        console.log(result);
+        map.flyTo(
+            {center: result, zoom: 15, speed: 0.2, curve: 1}
+        );
 
+        new mapboxgl.Marker()
+            .setLngLat(result)
+            .addTo(map);
+    });
+}
 
 //TODO: Using the geocode method above, add a marker at Codeup to the map
 //TODO: Instead of setCenter try using map.jumpTo()
@@ -66,3 +117,20 @@
 // TODO: Reverse geocode coordinates of your choice using the reverse geocode method
 
 
+/***
+ * reverseGeocode is a method to search for a physical address based on inputted coordinates
+ * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the string of the closest matching location to the coordinates provided
+ *
+ * EXAMPLE:
+ *
+ *  reverseGeocode({lat: 32.77, lng: -96.79}, API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+// reverseGeocode({lng: -98.4861, lat: 29.4260}, mapboxToken).then(function(results) {
+//     // logs the address for The Alamo
+//     console.log(results);
+// });
